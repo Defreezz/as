52cb2036cd9c3b23b6c2a4ee06a1dd6e17e6093c
Artsiom <yatsevich-artsiom@yandex.by>
2022-03-24 21:32:59 +0300

    index js

---
+++ dist/dts-01-main/routes.js
@@ -0,0 +1,6 @@
+"use strict";
+const payment = require('../dts-02-features/payment');
+const express = require('express');
+const router = express.Router();
+router.get('/', payment);
+module.exports = router;
