81f4830f90d021bbe7e9402fa5e94345a04729ab
Artsiom <yatsevich-artsiom@yandex.by>
2022-03-24 21:23:59 +0300

    procfile added

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import {Application} from "express";
 
 const express = require('express')
-const payment = require('./dts-01-main/routes')
+const payment = require('./src/dts-01-main/routes')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 
@@ -12,6 +12,6 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json())
 app.use("/payment",payment)
 
-app.listen(3000,()=>{
-    console.log('listen 3000')
+app.listen(process.env.PORT,()=>{
+    console.log(process.env.PORT)
 })
--- src/dts-01-main/routes.ts
+++ src/dts-01-main/routes.ts
@@ -1,4 +1,4 @@
-const payment = require('./dts-02-features/payment')
+const payment = require('../dts-02-features/payment')
 const express = require('express')
 
 const router = express.Router()
