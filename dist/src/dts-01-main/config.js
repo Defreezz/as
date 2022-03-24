aaa08f966d83c611dd0173345efd31613b5bbc86
Artsiom <yatsevich-artsiom@yandex.by>
2022-03-24 23:21:45 +0300

    mongodb added

---
+++ dist/src/dts-01-main/config.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.MongoDBUris = void 0;
+const USER_NAME = process.env.MONGO_DB_USER_NAME || "Defreezz";
+const PASSWORD = process.env.MONGO_DB_USER_PASSWORD || "1360674";
+const MONGO_DB_URL = process.env.MONGODB_URI || "cluster0.jrx2n.mongodb.net/Test"; // bd for tests
+exports.MongoDBUris = `mongodb+srv://${USER_NAME}:${PASSWORD}@${MONGO_DB_URL}?retryWrites=true&w=majority`;
